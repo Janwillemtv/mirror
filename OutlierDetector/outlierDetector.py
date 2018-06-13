7bfbdf3b81692c94086c025fd7b926d163071f8f
janwillemtv <janwillemtv1@gmail.com>
2018-06-13 16:19:41 +0200

    added functions and bad model

--- OutlierDetector/outlierDetector.py
+++ OutlierDetector/outlierDetector.py
@@ -7,6 +7,8 @@ import numpy as np
 import time
 import datetime as dt
 import os.path
+from os.path import dirname, abspath
+
 import pickle
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
@@ -15,7 +17,11 @@ from sklearn.datasets import fetch_mldata
 
 # import custom module
 from mnist_helpers import *
+from sklearn.model_selection import train_test_split
 
+
+
+#load data
 mnist = fetch_mldata('MNIST original', data_home='./')
 
 # minist object contains: data, COL_NAMES, DESCR, target fields
@@ -26,84 +32,103 @@ mnist.keys()
 images = mnist.data
 targets = mnist.target
 
-# Let's have a look at the random 16 images,
-# We have to reshape each data row, from flat array of 784 int to 28x28 2D array
-
-# pick  random indexes from 0 to size of our dataset
-#show_some_digits(images, targets)
-
-# ---------------- classification begins -----------------
-# scale data for [0,255] -> [0,1]
-# sample smaller size for testing
-# rand_idx = np.random.choice(images.shape[0],10000)
-# X_data =images[rand_idx]/255.0
-# Y      = targets[rand_idx]
 
 # full dataset classification
 X_data = images / 255.0
 Y = targets
 
+
+# split data to train and test
+# from sklearn.cross_validation import train_test_split
+
+
+X_train, X_test, y_train, y_test = train_test_split(X_data, Y, test_size=0.5, random_state=42)
+
+
 digitData = {}
 
 for digit in range(10):
     tempArray = []
-    for idx,label in enumerate(Y):
+    for idx,label in enumerate(y_test):
         if digit == label:
-            tempArray.append(X_data[idx])
+            tempArray.append(X_test[idx])
     digitData[digit] = tempArray
 
+############SELECT MODEL TO USE HERE##########
+## finalized_model.sav    this works best (doesnt create so much outliers)
+## finalized_model_bad    this creates more outliers (worse model)
+model = "finalized_model_bad.sav"
+
+classifier = load_model(model)
+predict_and_save(classifier)
 
 
-# split data to train and test
-# from sklearn.cross_validation import train_test_split
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X_data, Y, test_size=0.15, random_state=42)
 
 
 
 
-filename = "finalized_model.sav"
-if os.path.isfile(filename):
-    classifier = pickle.load(open(filename, 'rb'))
-    print("Loaded model")
-else:
-    ################ Classifier with good params ###########
-    #Create a classifier: a support vector classifier
-
-    param_C = 5
-    param_gamma = 0.05
-    classifier = svm.SVC(C=param_C,gamma=param_gamma)
-
-    #We learn the digits on train part
-    start_time = dt.datetime.now()
-    print('Start learning at {}'.format(str(start_time)))
-    classifier.fit(X_train, y_train)
-    end_time = dt.datetime.now()
-    print('Stop learning {}'.format(str(end_time)))
-    elapsed_time= end_time - start_time
-    print('Elapsed learning {}'.format(str(elapsed_time)))
-    filename = 'finalized_model.sav'
-    pickle.dump(classifier, open(filename, 'wb'))
 
 
-for digitToPredict in range(10):
+
+
+def load_model(model):
+    filepath = os.path.join("models", model)
+    if os.path.isfile(filepath):
+        classifier = pickle.load(open(filepath, 'rb'))
+        print("Loaded model")
+    else:
+        ################ Classifier with good params ###########
+        #Create a classifier: a support vector classifier
+        print("Model not found, training new one")
+        param_C = 5
+        param_gamma = 0.05
+        classifier = svm.SVC(C=param_C,gamma=param_gamma)
+
+        #We learn the digits on train part
+        start_time = dt.datetime.now()
+        print('Start learning at {}'.format(str(start_time)))
+        classifier.fit(X_train, y_train)
+        end_time = dt.datetime.now()
+        print('Stop learning {}'.format(str(end_time)))
+        elapsed_time= end_time - start_time
+        print('Elapsed learning {}'.format(str(elapsed_time)))
+        filename = 'finalized_model.sav'
+        pickle.dump(classifier, open(os.path.join("models",model), 'wb'))
+        return classifier
+
+
+def predict(classifier, digitToPredict):
+
     print("Predicting", digitToPredict)
-    tempOutliers = []
-    tempLabels = []
     X_test = digitData[digitToPredict]
 
     predicted = classifier.predict(X_test)
-    for idx, outcome in enumerate(predicted):
-        if outcome != digitToPredict: #outlier
-            tempOutliers.append(X_test[idx])
-            tempLabels.append(predicted[idx])
-    print("Nr of outliers", len(tempOutliers))
-    outliers = np.array(tempOutliers)
-    labels = np.array(tempLabels)
+    return predicted
 
-    print("Writing files")
 
-    np.save(os.path.join('outliers', str(digitToPredict)),outliers)
-    labelname = str(digitToPredict) + "_labels"
-    np.save(os.path.join('outliers', labelname),labels)
+def predict_and_save(classifier):
+    for digitToPredict in range(10):
+        print("Predicting", digitToPredict)
+        tempOutliers = []
+        tempGoodDigits = []
+        tempLabels = []
+        X_test = digitData[digitToPredict]
+
+        predicted = classifier.predict(X_test)
+        for idx, outcome in enumerate(predicted):
+            if outcome != digitToPredict: #outlier
+                tempOutliers.append(X_test[idx])
+                tempLabels.append(predicted[idx])
+            else:
+                tempGoodDigits.append(X_test[idx])
+        print("Nr of outliers", len(tempOutliers))
+        outliers = np.array(tempOutliers)
+        labels = np.array(tempLabels)
+        goodDigits = np.array(tempGoodDigits)
+
+        print("Writing files")
+
+        np.save(os.path.join('outliers', str(digitToPredict)),outliers)
+        labelname = str(digitToPredict) + "_labels"
+        np.save(os.path.join('outliers', labelname),labels)
+        np.save(os.path.join('goodDigits',str(digitToPredict)),goodDigits)
