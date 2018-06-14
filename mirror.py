e6189b04bdaa41da1178131ce8970006f5565357
Margot <github@margotrutgers.nl>
2018-06-14 16:02:23 +0200

    Add basic dashboard structure

--- mirror.py
+++ mirror.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.static_folder = 'static'
 
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to  m i r r o r'
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
